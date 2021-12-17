d52b4d34674f7e661982bd91731cc9e6a1a300c5
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-12-17 19:39:14 +0100

    Args

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -1,9 +1,16 @@
 public class Args {
     public static void main(String... args) {
 
+        int [] arr = new int[10];
+
+
         printUniversalMultiplicationResult(11, 2, 3, 4);
         printUniversalMultiplicationResult(1234567891, 2, 3, 4, 5, 6, 7);
 
+    }
+
+    private static  int varArgsExample(String[] arg,String[] varArgs){
+       return varArgs.length;
     }
 
     private static void printUniversalMultiplicationResult(long... args) {
@@ -13,4 +20,6 @@ public class Args {
         }
         System.out.println("Result equals: " + result);
     }
+
+
 }
