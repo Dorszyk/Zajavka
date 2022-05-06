d49c12d1f18ac1b2c81b3ae76c6a1183128a254c
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-05-06 18:40:31 +0200

    stringBuilder

---
+++ src/main/java/stringBuilder/Main2.java
@@ -0,0 +1,10 @@
+package stringBuilder;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello Wordl!");
+        stringBuilder.append("New");
+        System.out.println(stringBuilder);
+    }
+}
---
+++ src/main/java/stringBuilder/Main3.java
@@ -0,0 +1,13 @@
+package stringBuilder;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder(120);
+
+        System.out.println(stringBuilder.capacity());
+
+
+    }
+}
