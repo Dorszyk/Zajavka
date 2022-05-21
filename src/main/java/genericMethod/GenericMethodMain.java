c721b9353b9b744d79fbf9341b0eb35144f7460d
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-05-21 12:29:46 +0200

    inner&genericmethod

---
+++ src/main/java/genericMethod/GenericMethodMain.java
@@ -0,0 +1,44 @@
+package genericMethod;
+
+import java.util.Map;
+
+public class GenericMethodMain <T> {
+
+    private static Map<String ,Vehicle> vehicleMap = Map.of(
+            "Vehicle1", new Bike()
+    );
+
+    public static void main(String[] args) {
+        call1(new Bike());
+        call2(new Bike());
+
+        GenericMethodMain.<Bike>call3().getName();
+    }
+
+    public static <T extends  Vehicle> void call1(T element) {
+        System.out.println(element);
+    }
+
+    public static <T extends  Vehicle> T call2(T element){
+        System.out.println(element);
+        return element;
+    }
+
+    public static <T> T call3(){
+        return (T) vehicleMap.get("vehicle1");
+    }
+
+    interface Vehicle{
+
+    }
+
+    static class Bike implements  Vehicle{
+        String getName(){
+            return "bikeName";
+        }
+    }
+
+    static class Apple{
+
+    }
+}
