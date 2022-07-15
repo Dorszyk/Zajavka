7ca42a9d187c77b36514d740c62c7fd5a73c113d
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-07-15 18:53:00 +0200

    set&map

--- src/main/java/warsztat1_genericMethod/zadania/Zad1.java
+++ src/main/java/warsztat1_genericMethod/zadania/Zad1.java
@@ -6,13 +6,13 @@ public class Zad1 {
 // 4. Napisz metodę generyczną, która w podanej tablicy będzie w stanie zamienić ze sobą elementy na dwóch podanych pozycjach.
 
     public static void main(String[] args) {
-
         Cat cat1 = new Cat("jolka");
         Cat cat2 = new Cat("piotrek");
         Cat cat3 = new Cat("alunia");
 
         Cat[] catsArray = {cat1,cat2,cat3};
         System.out.println(Arrays.toString(catsArray));
+
         exercise(catsArray, 0, 2);
         System.out.println(Arrays.toString(catsArray));
 
