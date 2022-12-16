7ecea562acfc79c56a50453a8b84378d65f0c206
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-12-16 17:08:03 +0100

    ProjectV5

--- src/main/java/warsztat2_lambda_progFunkcyjne/project/MainV5.java
+++ src/main/java/warsztat2_lambda_progFunkcyjne/project/MainV5.java
@@ -1,2 +1,15 @@
-package warsztat2_lambda_progFunkcyjne.project;public class MainV5 {
+package warsztat2_lambda_progFunkcyjne.project;
+
+public class MainV5 {
+
+    /* Dla ludzi starszych niż 50 lat stwórz strukturę, w której zawrzesz informacje: rocznik, najmniej
+    popularna kategoria dla danego rocznika, ilość transakcji dla danego rocznika w obrębie danej
+    kategorii. Mówiąc najmniej popularna mamy na myśli, najmniejszą ilość dokonanych zakupów w
+    obrębie danej kategorii. Np: "rocznik: 62, najmniej popularna kategoria: GARDEN, transakcje: 5".
+    */
+
+    public static void main(String[] args) {
+
+
+    }
 }
